a3ad44e52e5b637229e440740a8209b0b03877af
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-01-24 15:42:33 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 //Write the function printO() here
+int printO(int N,int M){
+	if(M<=0 or N<=0){cout<<"Invalid input";}
+	else{
+	for(int i=0; i < N;i++){
+		for(int L=0;L<M;L++){
+			cout<<"O";
+		}
+		cout<<endl;
+	}
+	}
+}
+
+
+
+
 
 int main(){
 	
